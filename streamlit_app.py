60f249216c9457d24f19d772533627c152e494f0
nikitos2233 <nikita.lapsovs223@gmail.com>
2025-01-05 20:24:33 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.header("🧮 Simple usefulness for lasers")
 st.header("1. Calculation of laser wavelength")
 col1, col2 = st.columns([1, 3])
 with col1:
-    st.image("Images/energy.png", caption="Laser Energy", use_container_width=True)
+    st.image("Images/energy.png", caption="Laser Energy")
 with col2:
     f = st.number_input("Enter the frequency (Hz):", min_value=1.0, value=1e14, step=1e12, format="%.1e")
     c = 3e8  # Скорость света (м/с)
