ed922b7e6610508941b5ccca0606b08f06357cb4
nikitos2233 <nikita.lapsovs223@gmail.com>
2025-01-05 20:23:47 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,12 @@ st.write("Но что поделать")
 st.header("🧮 Simple usefulness for lasers")
 
 # 1. Расчёт длины волны лазера
+st.header("1. Calculation of laser wavelength")
 col1, col2 = st.columns([1, 3])
 with col1:
     st.image("Images/energy.png", caption="Laser Energy", use_container_width=True)
 with col2:
-    st.header("1. Расчёт длины волны лазера")
-    f = st.number_input("Введите частоту (Гц):", min_value=1.0, value=1e14, step=1e12, format="%.1e")
+    f = st.number_input("Enter the frequency (Hz):", min_value=1.0, value=1e14, step=1e12, format="%.1e")
     c = 3e8  # Скорость света (м/с)
     lambda_wave = c / f
-    st.write(f"**Результат:** Длина волны = {lambda_wave:.2e} м")
+    st.write(f"**Result:** wavelength = {lambda_wave:.3e} m")
